f4584b4f3df26ce795aec6fa1ab9c6b390ae9ac7
Nathaniel <314009+nathanielks@users.noreply.github.com>
2023-01-31 16:26:32 -0500

    make build executable

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -x
 
 cp ../MarlinConfigs/config/Configuration.h Marlin/Configuration.h
 cp ../MarlinConfigs/config/Configuration_adv.h Marlin/Configuration_adv.h
-pio run -e STM32G0B1RE_btt
+pio run
 cp .pio/build/STM32G0B1RE_btt/firmware.bin /Volumes/NO\ NAME/firmware.bin && echo 'Copied to SD Card'
 diskutil unmount /Volumes/NO\ NAME
